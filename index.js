52d38814a6a54822287bc9d267f9b2cfb7806e59
unknown <mahamuda3052@gmail.com>
2023-01-11 02:27:34 +0600

    added  rahad

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/',(req,res)=>{
     res.send('freemium node server is running')
 })
 app.get('/',(req,res)=>{
-    res.send('freemium node server is running 1')
+    res.send('freemium node server is running 2')
 })
 app.listen(port,()=>{
     console.log(`freemium node server running on port ${port}`)
