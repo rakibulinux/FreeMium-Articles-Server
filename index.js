d3819dc85cb309cd0f8ee054722cd660a0cf21bc
unknown <mahamuda3052@gmail.com>
2023-01-11 02:17:33 +0600

    added rakibul bai

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
     res.send('freemium node server is running')
 })
-
+app.get('/',(req,res)=>{
+    res.send('freemium node server is running 1')
+})
 app.listen(port,()=>{
     console.log(`freemium node server running on port ${port}`)
 })
