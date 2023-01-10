899b21a1607b9be3e44fa06e3566dc37b10d1134
unknown <mahamuda3052@gmail.com>
2023-01-11 02:31:03 +0600

    added bony

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/',(req,res)=>{
     res.send('freemium node server is running')
 })
 app.get('/',(req,res)=>{
-    res.send('freemium node server is running 2')
+    res.send('freemium node server is running 3')
 })
 app.listen(port,()=>{
     console.log(`freemium node server running on port ${port}`)
