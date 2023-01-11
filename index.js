32dc31c621708cdeea01e30870ea9f8efbea1d18
Rakib Linux <rakibulinux@gmail.com>
2023-01-11 16:08:43 +0600

    auth part done

---
+++ index.js
@@ -0,0 +1,126 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// Mongo DB Connections
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+//Verify JWT function
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(403).send("Not authorization");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: "Forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send(`FreeMium Articles running on port ${port}`);
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("freeMiumArticle").collection("users");
+
+    // Verfy Admin function
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const admin = await usersCollection.findOne(query);
+      if (admin?.role !== "admin") {
+        return res.status(403).send(`You dosen't have access to edit this`);
+      }
+      next();
+    };
+
+    app.put("/user/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          verify: true,
+        },
+      };
+
+      const updateUser = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
+      res.send(updateUser);
+    });
+
+    // Update users
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const updateUser = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ updateUser, token });
+    });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1d",
+        });
+        return res.send({ freeMiumToken: token });
+      }
+      res.status(401).send({ message: "Unauthorized" });
+    });
+
+    // Get admin user permission
+    app.get("/users/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const adminUser = await usersCollection.findOne(query);
+      res.send({ isAdmin: adminUser?.role === "admin" });
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+// Connection
+app.listen(port, () => {
+  console.log("API running in port: " + port);
+});
